daa10141a8ca862fb8f72883eccc8ee05a98ed7d
heavenrose <heavenrose9515@gmail.com>
2025-10-07 14:41:32 -0400

    bufwriter

--- src/main/java/com/pluralsight/PayrollCalculator.java
+++ src/main/java/com/pluralsight/PayrollCalculator.java
@@ -1,14 +1,30 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.*;
+import java.util.Scanner;
 
 public class PayrollCalculator {
     public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the name of the employee file to process: ");
+        String inputFileName = scan.nextLine();
+
+        System.out.print("Enter the name of the payroll file to create: ");
+        String outputFileName = scan.nextLine();
+
         try {
-            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+            FileReader fileReader = new FileReader(inputFileName);
             BufferedReader bufReader = new BufferedReader(fileReader);
+
+            FileWriter fileWriter = new FileWriter(outputFileName);
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+
+            bufWriter.write("employeeid |  employeeName | gross pay");
+            bufWriter.newLine();
 
 //            bufReader.readLine(); //Alternative method to skip first line of csv file
 
@@ -30,7 +46,11 @@ public class PayrollCalculator {
 
                     employeeList[count] = new Employee(id, name, hoursWorked, payRate);
 
-                    System.out.printf("EmployeeID: %d\nEmployee Name: %s\nGross Pay: $%.2f\n\n", employeeList[count].getEmployeeId(), employeeList[count].getEmployeeName(), employeeList[count].getGrossPay());
+                    bufWriter.write(employeeList[count].getEmployeeId()+"|"+employeeList[count].getEmployeeName()+"|"+String.format("%.2f",employeeList[count].getGrossPay()));
+                    bufWriter.newLine();
+
+                    bufWriter.close();
+                    System.out.println("Payroll file created:"+outputFileName);
 
                     count++;
                 }
